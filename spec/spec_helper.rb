ae7f5ade2ee5703c9cf9c66cd9c83f0e95d56ff7
Roshan <icemission@gmail.com>
2020-07-05 18:55:19 +0800

    added webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require "bundler/setup"
 require "api_test"
+require 'webmock/rspec'
+WebMock.disable_net_connect!
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure

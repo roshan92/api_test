b2a87433dc077be4f3279f39867503fa30a1315b
Roshan <icemission@gmail.com>
2020-07-05 19:08:44 +0800

    test to validate return response

--- lib/api_test.rb
+++ lib/api_test.rb
@@ -1,4 +1,21 @@
+require 'json'
+require 'net/http'
+require 'uri'
+
 module ApiTest
-  class Error < StandardError; end
-  # Your code goes here...
+  class APIError < StandardError; end
+  class APITimeoutError < APIError; end
+
+  class Service
+    HOST_URL = "https://biodata.herokuapp.com"
+
+    def get_resume
+      
+    end
+
+    private
+
+    def parsed_response
+    end
+  end
 end
--- spec/api_test_spec.rb
+++ spec/api_test_spec.rb
@@ -1,3 +1,32 @@
-RSpec.describe ApiTest do
+RSpec.describe "API Requests" do
 
+  let(:service) { ApiTest::Service.new }
+
+  describe "if returns a successful message" do
+    before do
+      url = "https://biodata.herokuapp.com"
+      body = {
+        "email": "icemission@gmail.com",
+        "first_name": "Roshan",
+        "last_name": "Tamil Sellvan"
+      }.to_json
+
+      @stub_api = stub_request(:get, url).to_return(status: 200, body: body)
+    end
+
+    it "validates the return response" do
+      resume = service.get_resume
+      expect(resume['email']).to eq('icemission@gmail.com')
+      expect(resume['first_name']).to eq('Roshan')
+      expect(resume['last_name']).to eq('Tamil Sellvan')
+    end
+  end
+
+  describe "if response returns empty body" do
+
+  end
+
+  describe "if request times out" do
+
+  end
 end

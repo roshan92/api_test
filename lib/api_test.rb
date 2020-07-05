7b96a2eebfd4e52b7d0c9a1f66fbc5a8b2f7c611
Roshan <icemission@gmail.com>
2020-07-05 19:12:51 +0800

    actual api call

--- lib/api_test.rb
+++ lib/api_test.rb
@@ -10,12 +10,21 @@ module ApiTest
     HOST_URL = "https://biodata.herokuapp.com"
 
     def get_resume
-      
+      resume = parsed_response
+      if resume.nil?
+        raise APIError, "Blank response"
+      end
+      resume
     end
 
     private
 
     def parsed_response
+      uri = URI(HOST_URL)
+      response = Net::HTTP.get_response(uri)
+      !response.body.empty? ? JSON.parse(response.body) : nil
+    rescue Timeout::Error => e do
+      raise APITimeoutError, e.message
     end
   end
 end

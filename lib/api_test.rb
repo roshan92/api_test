6805ce9d2642b775fdb380f091dc3b574a13b7d1
Roshan <icemission@gmail.com>
2020-07-05 19:17:46 +0800

    fix typo

--- lib/api_test.rb
+++ lib/api_test.rb
@@ -11,6 +11,7 @@ module ApiTest
 
     def get_resume
       resume = parsed_response
+      raise APIError unless resume
       if resume.nil?
         raise APIError, "Blank response"
       end
@@ -23,7 +24,7 @@ module ApiTest
       uri = URI(HOST_URL)
       response = Net::HTTP.get_response(uri)
       !response.body.empty? ? JSON.parse(response.body) : nil
-    rescue Timeout::Error => e do
+    rescue Timeout::Error => e
       raise APITimeoutError, e.message
     end
   end
